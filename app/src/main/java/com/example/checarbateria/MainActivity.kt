f77d5d17d3ea07ed9d7efdaf5c647d2be1b5c413
luis.freire <luis.freire@academico.ifpb.edu.br>
2022-05-11 15:42:19 -0300

    commit1

---
+++ app/src/main/java/com/example/checarbateria/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.checarbateria
+
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var tvStatus: TextView
+    private lateinit var botao: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.tvStatus = findViewById(R.id.tvStatus)
+        this.botao = findViewById(R.id.botao)
+
+       this.botao.setOnClickListener {
+           val batteryStatus: Intent? = IntentFilter(Intent.ACTION_BATTERY_CHANGED).let { ifilter ->
+               this.registerReceiver(null, ifilter)
+           }
+
+           val status: Int = batteryStatus?.getIntExtra(BatteryManager.EXTRA_STATUS, -1) ?: -1
+           val statusCarregamento: Boolean = status == BatteryManager.BATTERY_STATUS_CHARGING
+
+           Log.i("CHECA_BATERIA", statusCarregamento.toString())
+           if (statusCarregamento) {
+               this.tvStatus.text = "Carregando!"
+           } else {
+               this.tvStatus.text = "Não está carregando!"
+           }
+       }
+    }
+}
